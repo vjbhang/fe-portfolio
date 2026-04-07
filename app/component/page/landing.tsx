62ba72d15a5bc815f44a4fb92c1e0c0ff4ed4825
vjbhang <bhangjiwon@pm.me>
2026-04-07 16:13:34 -0700

    Add ShakyText component and update landing page for enhanced visual effects and user interaction

--- app/component/page/landing.tsx
+++ app/component/page/landing.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import ShakyText from "../elements/ShakyText";
 
 export default function Landing() {
   return (
@@ -13,18 +14,37 @@ export default function Landing() {
             -- Because Every Detail Matters
           </h5>
         </div>
-        <div className="flex flex-col bg-black/50 items-center justify-center w-78.5 h-70 rounded-lg gap-4 ml-[6vw]">
-          <Image
-            src="/scrollmouseSVG.svg"
-            alt="scroll mouse"
-            width={62}
-            height={127}
-            className="w-24 h-24"
-          />
+        <div className="flex flex-col bg-black/50 items-center justify-center w-120 h-70 rounded-lg gap-4 ml-[6vw]">
           <div className="flex flex-col items-center gap-1 text-center text-white text-lg">
-            <p>Begin scrolling down</p>
-            <p>to initiate</p>
-            <p className="text-launchred">Spaceship Launch</p>
+            <div className="flex justify-start items-center flex-row gap-40 mr-auto">
+              <Image
+                src="/scrollmouseSVG.svg"
+                alt="scroll mouse"
+                width={62}
+                height={127}
+                className="w-10 h-10 ml-10"
+              />
+              <div className="flex flex-row items-center gap-1">
+                <Image
+                  src="/arrow-left-keypad.svg"
+                  alt="left arrow key"
+                  width={62}
+                  height={127}
+                  className="w-6 h-6"
+                />
+                <Image
+                  src="/arrow-right-keypad.svg"
+                  alt="right arrow key"
+                  width={62}
+                  height={127}
+                  className="w-6 h-6"
+                />
+              </div>
+            </div>
+            <p>Scroll down or use left/right arrow keys to</p>
+            <p className="text-amber-500 font-bold text-xl font-inconsolata text-center">
+              <ShakyText content={"INITIATE LAUNCH"} />
+            </p>
           </div>
         </div>
       </div>
