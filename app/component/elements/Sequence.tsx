d73ad891e4fd9b10d61b032138ed22802422d7f2
vjbhang <bhangjiwon@pm.me>
2026-04-07 16:16:59 -0700

    debug: JSX.Element TS import

--- app/component/elements/Sequence.tsx
+++ app/component/elements/Sequence.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import Image from "next/image";
 import MouseScroll from "./MouseScroll/MouseScroll";
 import ShakyText from "./ShakyText";
